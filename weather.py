3efb0424a8be91318360af7093c6857de4590e80
Tony Liu <tl0226yn@gmail.com>
2022-01-09 18:29:56 -0800

    Cleans up code

--- weather.py
+++ weather.py
@@ -11,7 +11,7 @@ def forecast(city):
         data = requests.get(url).content
         b4 = BeautifulSoup(data, 'lxml')
 
-        #prints out the current temperature
+        #prints out the current temperature and weather condition for a specified city
         g = b4.find_all('div')
         if len(g) == 0:
             raise Exception("Invalid city entered")
@@ -19,16 +19,8 @@ def forecast(city):
         currentTemp = b4.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
         currentTime = b4.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
         forecast = b4.find('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'}).text
-        print("The weather for " + city + ": " + currentTime + ". Temperature: " +currentTemp)
-        print("Your 3 day forecast is: " +forecast)
-        #prints out the current weather condition
-        print(g)
-    
-        #prints out the weather and temperature for today
-
-
-        #prints out the weather and temperature for tomorrow
-
+        print(f"The weather for {city}: {currentTime}. Temperature: {currentTemp}")
+        
     except Exception as e:
         print("Invalid city name entered")
         print(e)
